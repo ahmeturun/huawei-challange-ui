654aaa84579ddf63f4db305f2af7973a71053663
AHMET BEKIR URUN <ahmet.urun@outlook.com>
2019-07-27 13:22:45 +0300

    initial commit.

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class App extends Component{
+   render(){
+      return(
+         <div>
+            <h1>Header</h1>
+            <h2>Content</h2>
+         </div>
+      );
+   }
+}
+export default App;
